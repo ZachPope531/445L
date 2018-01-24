3f0b0d12424fe202ef116a7cecb37822b75eee2d
ZachPope531 <zachary.pope531@gmail.com>
2018-01-24 16:00:58 -0600

    Added more plot stuff

--- Lab1/Lab1/fixed.c
+++ Lab1/Lab1/fixed.c
@@ -5,8 +5,21 @@
 #include "../inc/tm4c123gh6pm.h"
 #include "fixed.h"
 
+int32_t plot_minX;
+int32_t plot_maxX;
+int32_t plot_minY;
+int32_t plot_maxY;
+
 void ST7735_XYplotInit(char *title, int32_t minX, int32_t maxX, int32_t minY, int32_t maxY){
 	Output_Init();
+	ST7735_SetCursor(0,0);
+	for(int i = 0; title[i] != NULL; i++){
+		ST7735_OutChar(title[i]);
+	}
+	plot_minX = minX;
+	plot_maxX = maxX;
+	plot_minY = minY;
+	plot_maxY = maxY;
 }
 
 void ST7735_XYplot(uint32_t num, int32_t bufX[], int32_t bufY[]){
