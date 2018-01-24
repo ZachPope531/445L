d7991811bb027e6388adf34257776a9b485a07ba
ZachPope531 <zachary.pope531@gmail.com>
2018-01-23 23:50:36 -0600

    Updated XYPlot function

--- Lab1/Lab1/fixed.c
+++ Lab1/Lab1/fixed.c
@@ -10,5 +10,18 @@ void ST7735_XYplotInit(char *title, int32_t minX, int32_t maxX, int32_t minY, in
 }
 
 void ST7735_XYplot(uint32_t num, int32_t bufX[], int32_t bufY[]){
-	
+	int32_t x_coord_32b;
+	int32_t y_coord_32b;
+	int16_t x_coord_16b;
+	int16_t y_coord_16b;
+	for(uint16_t i = 0; i < num; i++){
+		x_coord_32b = bufX[i];
+		y_coord_32b = bufY[i];
+		//Have to convert fixed point decimal to something usable
+		//Map the values to the 160x128 screen?
+		ST7735_DrawPixel(x_coord_16b, y_coord_16b, 0xFFFF);
+		ST7735_DrawPixel(x_coord_16b + 1, y_coord_16b, 0xFFFF);
+		ST7735_DrawPixel(x_coord_16b, y_coord_16b + 1, 0xFFFF);
+		ST7735_DrawPixel(x_coord_16b + 1, y_coord_16b + 1, 0xFFFF);
+	}
 }
