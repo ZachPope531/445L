b3f06bb55bb27ddf87abc7367b64ebc599b5a330
Ali Kedwaii <ali.kedwaii@utexas.edu>
2018-02-11 20:13:03 -0600

    testin timer0

--- Lab3/Lab3/timer.c
+++ Lab3/Lab3/timer.c
@@ -1,3 +1,104 @@
 #include "timer.h"
 
-//Just copy and past Timer0 code here from lab 2 or the ValvanoWare pack
+#define PF1             (*((volatile uint32_t *)0x40025008))
+	
+volatile uint16_t hours;
+volatile uint16_t minutes;
+volatile uint16_t seconds;
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+long StartCritical (void);    // previous I bit, disable interrupts
+void EndCritical(long sr);    // restore I bit to previous value
+void WaitForInterrupt(void);  // low power mode
+
+
+enum part {
+	HR,
+	MIN,
+	SEC
+};
+
+enum mode {
+	TIME,
+	ALARM,
+	DISPLAY
+};
+
+enum timemode {
+	AMPM,
+	MILITARY
+};
+
+enum displaymode {
+	ANALOG,
+	DIGITAL
+};
+
+int part, mode, timemode, displaymode;
+
+void Timer0A_Init100HzInt(void){
+  volatile uint32_t delay;
+  DisableInterrupts();
+  // **** general initialization ****
+  SYSCTL_RCGCTIMER_R |= 0x01;      // activate timer0
+  delay = SYSCTL_RCGCTIMER_R;      // allow time to finish activating
+  TIMER0_CTL_R &= ~TIMER_CTL_TAEN; // disable timer0A during setup
+  TIMER0_CFG_R = 0;                // configure for 32-bit timer mode
+  // **** timer0A initialization ****
+                                   // configure for periodic mode
+  TIMER0_TAMR_R = TIMER_TAMR_TAMR_PERIOD;
+  TIMER0_TAILR_R = 7999;         // start value for 1 Hz interrupts
+  TIMER0_IMR_R |= TIMER_IMR_TATOIM;// enable timeout (rollover) interrupt
+  TIMER0_ICR_R = TIMER_ICR_TATOCINT;// clear timer0A timeout flag
+  TIMER0_CTL_R |= TIMER_CTL_TAEN;  // enable timer0A 32-b, periodic, interrupts
+  // **** interrupt initialization ****
+                                   // Timer0A=priority 1
+  NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x20000000; // top 3 bits
+  NVIC_EN0_R = 1<<19;              // enable interrupt 19 in NVIC
+}
+
+void Timer0A_Handler(void){
+  TIMER0_ICR_R = TIMER_ICR_TATOCINT;    // acknowledge timer0A timeout
+  seconds++;
+	if (seconds >= 60) {
+		seconds = 0;
+		minutes++;
+	}
+	if (minutes >= 60) {
+		minutes = 0;
+		hours++;
+	}
+	if (hours >= 24) {
+		hours = 0;
+	}
+	
+	if (displaymode == ANALOG){
+		drawHands(hours, minutes, seconds);
+	} else if (displaymode == DIGITAL) {
+		digitalClock(hours, minutes, seconds);
+	}
+}
+ /*
+void Timer1_Init(void){
+  SYSCTL_RCGCTIMER_R |= 0x02;   // 0) activate TIMER1
+  //PeriodicTask = task;          // user function
+  TIMER1_CTL_R = 0x00000000;    // 1) disable TIMER1A during setup
+  TIMER1_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER1_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER1_TAILR_R = 0xFFFFFFFF;    // 4) reload value
+  TIMER1_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER1_ICR_R = 0x00000001;    // 6) clear TIMER1A timeout flag
+  TIMER1_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0xFFFF00FF)|0x00004000; // 8) priority 2
+// interrupts enabled in the main program after all devices initialized
+// vector number 37, interrupt number 21
+  NVIC_EN0_R = 1<<21;           // 9) enable IRQ 21 in NVIC
+  TIMER1_CTL_R = 0x00000001;    // 10) enable TIMER1A
+}
+
+void Timer1A_Handler(void){
+  TIMER1_ICR_R = TIMER_ICR_TATOCINT;// acknowledge TIMER1A timeout
+  
+}
+*/
---
+++ Lab3/Lab3/timer.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+#include "PLL.h"
+#include "screen.h"
+
+
+void Timer0A_Init100HzInt(void);
+
+void Timer0A_Handler(void);
+
