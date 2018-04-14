e6439b656914b12a21e3368007e55372438a66e8
ZachPope531 <zachary.pope531@gmail.com>
2018-04-13 23:43:09 -0500

    Calib.h

---
+++ Lab9/Calib.h
@@ -0,0 +1,10 @@
+/**calib.h
+Zachary Pope and Ali Kedwaii
+Saadallah Kassir, the brightest star in the darkest night
+
+100k/25C:	1246
+500k/0C	:	4037
+50k/40C	:	9
+200k/10C:	3232
+71k/32C	:	574
+**/
